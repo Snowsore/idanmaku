8b7f9863bc20c69bf01f55d479ad6359eeb93cf3
Snowsore <38675291+Snowsore@users.noreply.github.com>
2019-04-25 19:49:03 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+// Iframe danmaku server
+
+const express = require('express');
+const app = express();
+
+const pug = require('pug');
+app.set('view engine', 'pug');
+
+const http = require('http').Server(app);
+
+const io = require('socket.io')(http);
+
+app.get('/cannon/*', (req,res) => {
+	res.render('cannon');
+});
+
+app.get('/*', (req, res) => {
+	res.render('canvas');
+});
+
+io.on('connection', (socket) => {
+	socket.on('joinRoom', (room) => {
+		socket.room = room;
+		socket.join(room);
+	});
+
+	socket.on('fire', (msg) => {
+		io.in(socket.room).emit('fired', msg);
+	});
+});
+
+http.listen(8096);

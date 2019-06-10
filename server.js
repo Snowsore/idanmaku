a1126384a57aa111c60852fe59ea292363601140
Snowsore <38675291+Snowsore@users.noreply.github.com>
2019-06-11 06:13:57 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -9,15 +9,22 @@ const pug = require('pug');
 app.set('view engine', 'pug');
 
 const http = require('http').Server(app);
-
 const io = require('socket.io')(http);
 
 app.get('/cannon/*', (req,res) => {
 	res.render('cannon');
 });
 
-app.get('/*', (req, res) => {
+app.get('/canvas/*', (req, res) => {
 	res.render('canvas');
+});
+
+app.get('/js/*', (req, res) => {
+	res.sendFile(__dirname + req.path);
+});
+
+app.get('*', (req, res) => {
+	res.render('index');
 });
 
 io.on('connection', (socket) => {
